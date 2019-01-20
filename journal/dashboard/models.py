c4b60fa34f436c8ea68a8793b97a6fa88f6b2422
Ivan <ivanleomk@gmail.com>
2019-01-20 11:10:16 +0800

    App V2.1
    I am giving up

--- journal/dashboard/models.py
+++ journal/dashboard/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import datetime
 
+
 class entry(models.Model):
     entry = models.CharField(max_length=200)
     entry_date = models.DateTimeField(auto_now_add=True)
--- journal/dashboard/views.py
+++ journal/dashboard/views.py
@@ -4,14 +4,16 @@ from django.template import loader
 from dashboard.models import entry
 from django.views.generic import TemplateView
 import datetime
+from .models import entry
 # Create your views here.
 
-
+#Main Dashboard page
 def index(request):
     template = loader.get_template('dashboard/index.html')
     context = {}
     return HttpResponse(template.render(context,request))
 
+#Entries Page
 def entries(request):
     template = loader.get_template('dashboard/entries.html')
     entries = entry.objects.all()
@@ -23,12 +25,22 @@ def entries(request):
     entries = {'entry':values}
     return render(request, 'dashboard/entries.html', {"entries": values})
 
+#Account Page
 def account(request):
     template = loader.get_template('dashboard/account.html')
     context = {}
     return HttpResponse(template.render(context,request))
 
+#New Entry!
 def newentry(request):
     template = loader.get_template('dashboard/newentry.html')
-    context = {}
-    return HttpResponse(template.render(context,request))
+    if request.method == 'POST':
+        if request.POST.get('entry'):
+            post=entry()
+            post.entry= request.POST.get('entry')
+            post.save()
+            return render(request, 'dashboard/index.html')
+        else:
+            return render(request,'dashboard/entries.html')
+    else:
+        return render(request,'dashboard/newentry.html')
