3d75b7e26bdd81254b94e6351d85660e0f8cb104
Ivan <ivanleomk@gmail.com>
2019-01-20 12:41:26 +0800

    It works

--- journal/dashboard/models.py
+++ journal/dashboard/models.py
@@ -7,6 +7,5 @@ class entry(models.Model):
     entry = models.CharField(max_length=200)
     entry_date = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.CharField(max_length=200)
     def __str__(self):
         return self.entry
