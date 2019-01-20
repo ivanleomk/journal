a4039ab2372e348900a8eaa7698ea31cca873f8a
Ivan <ivanleomk@gmail.com>
2019-01-20 10:02:48 +0800

    App V2.1
    Changes
    * Displays the data from the database and renders it in the webpage

--- journal/dashboard/views.py
+++ journal/dashboard/views.py
@@ -2,22 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from dashboard.models import entry
-
+from django.views.generic import TemplateView
+import datetime
 # Create your views here.
 
 
 def index(request):
     template = loader.get_template('dashboard/index.html')
-    context = {
-    }
+    context = {}
     return HttpResponse(template.render(context,request))
 
 def entries(request):
     template = loader.get_template('dashboard/entries.html')
     entries = entry.objects.all()
-    content = {}
-    #context = {[1,"today was a good day","12-03-17"],[2,"today was an good day","12-03-17"],[3,"today was and good day","12-03-17"]}
-    return HttpResponse(template.render(context,request))
+    values = []
+    for n in entries.values():
+        date = n['entry_date'].strftime('%m/%d/%Y')
+        values.append([date,n['entry']])
+    print(values)
+    entries = {'entry':values}
+    return render(request, 'dashboard/entries.html', {"entries": values})
 
 def account(request):
     template = loader.get_template('dashboard/account.html')
